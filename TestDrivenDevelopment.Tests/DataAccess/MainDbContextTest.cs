af30566dbf0cf3dbdddbc95c3babe90cd81e2576
Emre Can Cakir <emreecann28@gmail.com>
2024-04-17 08:58:06 +0300

    feat(dbContext): create and write test and logic db context and generate migration

---
+++ TestDrivenDevelopment.Tests/DataAccess/MainDbContextTest.cs
@@ -0,0 +1,30 @@
+﻿using EntityFrameworkCore.Testing.Moq;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDrivenDevelopmentApp.DataAccess;
+using TestDrivenDevelopmentApp.Model;
+
+namespace TestDrivenDevelopment.Tests.DataAccess
+{
+    public class MainDbContextTest
+    {
+        [Fact]
+        public void DbContext_ShouldWithDbContextOptions_IsAvailable()
+        {
+            var mockedDbContext = Create.MockedDbContextFor<MainDbContext>();
+            mockedDbContext.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void DbContext_DbSets_MustHaveDbSetWithTypeBookEntity()
+        {
+            var mockedDbContext = Create.MockedDbContextFor<MainDbContext>();
+            Assert.True(mockedDbContext.Books is DbSet<Book>);
+        }
+    }
+}
---
+++ TestDrivenDevelopmentApp/DataAccess/MainDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestDrivenDevelopmentApp.Model;
+
+namespace TestDrivenDevelopmentApp.DataAccess
+{
+    public class MainDbContext : DbContext
+    {
+        public MainDbContext()
+        {
+        }
+        public MainDbContext(DbContextOptions<MainDbContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Books { get; set; }
+    }
+}
