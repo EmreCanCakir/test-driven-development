66cfacabd7564d5c568efa4ab15fdc7fbf809183
Emre Can Cakir <emreecann28@gmail.com>
2024-04-22 21:50:22 +0300

    feat(AuthTokenConsumer): create consumer classes and publish and comsume event

---
+++ AuthManagement/Consumer/AuthTokenCreateConsumer.cs
@@ -0,0 +1,24 @@
+﻿using AuthManagement.Services;
+using Contracts;
+using MassTransit;
+
+namespace AuthManagement.Consumer
+{
+    public sealed class AuthTokenCreateConsumer : IConsumer<AuthTokenCreateEvent>
+    {
+        private readonly ITokenService _tokenService;
+        private readonly IPublishEndpoint _publishEndpoint;
+        public AuthTokenCreateConsumer(ITokenService tokenService, IPublishEndpoint publishEndpoint)
+        {
+            _tokenService = tokenService;
+            _publishEndpoint = publishEndpoint;
+        }
+
+        public Task Consume(ConsumeContext<AuthTokenCreateEvent> context)
+        {
+            var token = _tokenService.GenerateJwtToken(context.Message.UserName, context.Message.Password);
+            _publishEndpoint.Publish(new AuthTokenGeneratedEvent { Token = token}, context.CancellationToken);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ TestDrivenDevelopmentApp/Consumer/AuthTokenGeneratedConsumer.cs
@@ -0,0 +1,22 @@
+﻿using Contracts;
+using MassTransit;
+
+namespace TestDrivenDevelopmentApp.Consumer
+{
+    public sealed class AuthTokenGeneratedConsumer : IConsumer<AuthTokenGeneratedEvent>
+    {
+        private readonly TaskCompletionSource<string> _tokenCompletionSource = new TaskCompletionSource<string>();
+
+        public Task Consume(ConsumeContext<AuthTokenGeneratedEvent> context)
+        {
+            _tokenCompletionSource.TrySetResult(context.Message.Token);
+            Console.WriteLine($"Token generated: {context.Message.Token}");
+            return Task.CompletedTask;
+        }
+
+        public Task<string> GetToken()
+        {
+            return _tokenCompletionSource.Task;
+        }
+    }
+}
